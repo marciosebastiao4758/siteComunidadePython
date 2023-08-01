b631ef82596d0383afb7dad6bd7ce7cbc8ab3dc9
Márcio Sebastião Martins <54046442+marciosebastiao4758@users.noreply.github.com>
2023-08-01 18:08:48 -0300

    Update __init__.py

--- comunidadeImpressonadora/__init__.py
+++ comunidadeImpressonadora/__init__.py
@@ -2,6 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+
+app = Flask(__name__)
+lista_usuarios = ["Márcio", "Felipe", "Matheus", "Alexandre", "Sandra"]
+app.config["SECRET_KEY"] = 'd25ad6e2701f3459a5898f5896e3b891'
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///comunidade.db'
+
+
+
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "alert-info"
+
+
+from comunidadeImpressonadora import routes
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 lista_usuarios = ["Márcio", "Felipe", "Matheus", "Alexandre", "Sandra"]
